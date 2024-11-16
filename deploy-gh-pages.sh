1465270265c5df6b01f1d1be686fccc776f40fcd
Jack Koppa <hello@jackkoppa.com>
2024-11-16 17:09:53 -0500

    fix: correct branch naming during deploy script

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,6 +1,6 @@
 ORIGINAL_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 git branch -D gh-pages
-git checkout branch gh-pages
+git checkout -b gh-pages
 pnpm build-gh-pages
 git add .
 git commit -m "deploy: gh-pages"
